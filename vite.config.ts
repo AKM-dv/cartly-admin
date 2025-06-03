7ca4c2ef97e736a466d863322972f61be543b40c
AKM-dv <120122492+AKM-dv@users.noreply.github.com>
2025-06-04 00:34:47 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cartly-admin/', // 👈 important!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
